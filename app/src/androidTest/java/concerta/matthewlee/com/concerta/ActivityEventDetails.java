4fcdd6ad0c25919b2e43057f566825f0293f8787
Matthew Lee <matthew.lee711@gmail.com>
2016-07-14 10:36:25 -0500

    Completed UI: Details, list view, and main

---
+++ app/src/androidTest/java/concerta/matthewlee/com/concerta/ActivityEventDetails.java
@@ -0,0 +1,13 @@
+package concerta.matthewlee.com.concerta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityEventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_event_details);
+    }
+}
